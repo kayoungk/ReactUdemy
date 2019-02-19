2538e0dfca5af4cb8661c42835ca773e1e6a0a86
Kayoung <tat.tot.tat@gmail.com>
2019-02-19 16:01:22 +1100

    [master] props added in Person class
    added class Person, passes props

--- complete-guide/src/App.js
+++ complete-guide/src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';// Use uppercase. Lowercase is reserved for JSX.
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Hi, I'm a React App.</h1>
+        <p>This is really working.</p>
+        <h1>Test</h1>
+        <Person name="Kayoung" age="24"/>
+        <Person name="Kaiqi" age="25" > My hobby is racing.</Person>
+        <Person name="Wanwan" age="4"/>
       </div>
     );
+    // return React.createElement('div',{className: "App"}, React.createElement('h1',null, 'The same effect'))
   }
 }
 
